2b2d3eeaeede326017d36015acb6a86915df9ece
Scott Erickson <sderickson@gmail.com>
2025-03-26 12:05:42 -0700

    bring over implementation

--- auth-db/src/instance.ts
+++ auth-db/src/instance.ts
@@ -4,6 +4,8 @@ import * as schema from "./schema.ts";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import { AuthDatabaseError } from "./errors.ts";
 import { getDbPath, getMigrationsPath } from "../drizzle.config.ts";
+import { createUserQueries } from "./queries/users.ts";
+import { createEmailAuthQueries } from "./queries/email-auth.ts";
 
 export interface AuthDBConfig {
   dbPath?: string;
@@ -28,6 +30,8 @@ export class AuthDB {
 
       // Add query methods to this instance
       Object.assign(this, db);
+      Object.assign(this, createUserQueries(db));
+      Object.assign(this, createEmailAuthQueries(db));
     } catch (error) {
       throw new AuthDatabaseError(
         `Failed to initialize database: ${error instanceof Error ? error.message : String(error)}`
