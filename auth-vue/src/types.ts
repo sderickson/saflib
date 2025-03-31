7ac5926d4a915abdcf559f3104d66f482746fb1c
Scott Erickson <sderickson@gmail.com>
2025-03-31 11:00:07 -0700

    Add tanstack queries

--- auth-vue/src/types.ts
+++ auth-vue/src/types.ts
@@ -3,3 +3,7 @@ import type { components } from "@saflib/auth-spec";
 export type LoginRequest = components["schemas"]["LoginRequest"];
 export type RegisterRequest = components["schemas"]["RegisterRequest"];
 export type UserResponse = components["schemas"]["UserResponse"];
+export type ForgotPasswordRequest =
+  components["schemas"]["ForgotPasswordRequest"];
+export type ResetPasswordRequest =
+  components["schemas"]["ResetPasswordRequest"];
