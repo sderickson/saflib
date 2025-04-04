dcf38714c57c5f08c3f372e78908fe4610f5f9c0
Scott Erickson <sderickson@gmail.com>
2025-04-04 08:19:20 -0700

    simplified csrf.ts

--- auth-service/csrf.ts
+++ auth-service/csrf.ts
@@ -1,57 +1,39 @@
-"use strict";
+// simplified, typed version of https://www.npmjs.com/package/express-csrf-double-submit-cookie
 
 import crypto from "crypto";
 import type { CookieOptions, NextFunction, Request, Response } from "express";
-const DEFAULT_OPTIONS = {
-  length: 24,
-  value: defaultValue,
-};
 
-const DEFAULT_COOKIE_OPTIONS = {
+const DEFAULT_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: false,
-  name: "_csrf_token",
   path: "/",
 };
 
-/**
- * Simple middleware for "double submit cookie" CSRF protection method.
- * @see https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet#Double_Submit_Cookie
- *
- * @param {object} options
- * @param {number} [options.length=24] Token length in bytes.
- * @param {function} [options.value=24] Function to get token from request (defaults: body._csrf_token || query._csrf_token || headers['x-csrf-token'])
- * @param {cookie} [options.cookie] Cookie options, see express res.cookie() documentation. Default: { name: '_csrf_token', path: '/' }
- */
 type Options = {
   length?: number;
-  value?: (req: Request) => string;
   cookie?: CookieOptions;
 };
 
+const DEFAULT_LENGTH = 24;
+const NAME = "_csrf_token";
+
 export const csrfDSC = (options: Options = {}) => {
-  const optionsWithDefaults = {
-    ...DEFAULT_OPTIONS,
-    ...options,
-  };
   const cookieOptions = {
     ...DEFAULT_COOKIE_OPTIONS,
-    ...optionsWithDefaults.cookie,
+    ...options.cookie,
   };
 
   const middleware = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.cookies[cookieOptions.name]) {
+    if (!req.cookies[NAME]) {
       const csrfToken = crypto
-        .randomBytes(optionsWithDefaults.length)
+        .randomBytes(options.length || DEFAULT_LENGTH)
         .toString("hex");
-      req.cookies[cookieOptions.name] = csrfToken;
-      res.cookie(cookieOptions.name, csrfToken, cookieOptions);
+      req.cookies[NAME] = csrfToken;
+      res.cookie(NAME, csrfToken, cookieOptions);
     }
 
     req.isValidCsrfToken = () => {
-      // console.log("validating csrf token", req.cookies[cookieOptions.name]);
-      // console.log("options.value", options.value(req));
-      const cookieToken = req.cookies[cookieOptions.name];
-      const requestToken = optionsWithDefaults.value(req);
+      const cookieToken = req.cookies[NAME];
+      const requestToken = getCsrfToken(req);
 
       return Boolean(
         requestToken && cookieToken && cookieToken === requestToken,
@@ -69,7 +51,7 @@ export const csrfDSC = (options: Options = {}) => {
  * @param {IncomingMessage} req
  * @return {string}
  */
-function defaultValue(req: Request) {
+function getCsrfToken(req: Request) {
   return (
     req.headers["x-csrf-token"] ||
     (req.body && req.body._csrf_token) ||
