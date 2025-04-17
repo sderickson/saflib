d7ebaacd7db0d99ccbeb21ff34cf732d1071f23b
Scott Erickson <sderickson@gmail.com>
2025-04-17 07:32:49 -0700

    Add explanation

--- vitest/vitest.config.js
+++ vitest/vitest.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from "vitest/config";
 
 export const defaultConfig = defineConfig({
   test: {
+    /*
+     * By default, isolate tests. There's no apparent change in performance, but sometimes tests
+     * will break in CI or when you run them locally with --no-file-parallelism, particularly when
+     * a set of tests require different vi.mock setups. For example, one test might mock an
+     * external dependency, but another runs first and imports the file that would receive the mock.
+     * It will not because that file will have already been loaded.
+     *
+     * So, removing test isolation is opt-in. Seems best when testing purely functional code. No
+     * mocks, no globals, no side effects.
+     */
+    isolate: true,
+
     environment: "node",
     include: ["**/*.test.ts"],
     coverage: {
