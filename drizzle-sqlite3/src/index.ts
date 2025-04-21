ad7b4e3c927842ab877acf159f183a3e70e3c8a8
Scott Erickson <sderickson@gmail.com>
2025-04-21 16:03:30 -0700

    trying out a lightweight query typing approach

--- drizzle-sqlite3/src/index.ts
+++ drizzle-sqlite3/src/index.ts
@@ -1,3 +1,7 @@
+export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
+  ? Pick<T, K> & Partial<Record<Exclude<keyof T, K>, never>>
+  : never;
+
 export {
   queryWrapper,
   UnhandledDatabaseError,
