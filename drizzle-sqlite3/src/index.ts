4cbfd5b23466e1b059542ce5c142d2d11d99b5b6
Scott Erickson <sderickson@gmail.com>
2025-04-22 09:33:32 -0700

    move OneOf to monorepo pkg

--- drizzle-sqlite3/src/index.ts
+++ drizzle-sqlite3/src/index.ts
@@ -1,7 +1,3 @@
-export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
-  ? Pick<T, K> & Partial<Record<Exclude<keyof T, K>, never>>
-  : never;
-
 export {
   queryWrapper,
   UnhandledDatabaseError,
---
+++ monorepo/index.ts
@@ -0,0 +1,3 @@
+export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
+  ? Pick<T, K> & Partial<Record<Exclude<keyof T, K>, never>>
+  : never;
