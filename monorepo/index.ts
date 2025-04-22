3086bf994254d3b290cef9af0dd7665525be6ca5
Scott Erickson <sderickson@gmail.com>
2025-04-22 13:08:54 -0700

    stop exporting OneOf

--- monorepo/index.ts
+++ monorepo/index.ts
@@ -1,4 +1,4 @@
-export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
+type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
   ? Pick<T, K> & Partial<Record<Exclude<keyof T, K>, never>>
   : never;
 
