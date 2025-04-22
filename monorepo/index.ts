9b58e3e3bab24e345302167f50c798a51de08f5d
Scott Erickson <sderickson@gmail.com>
2025-04-22 13:02:07 -0700

    add a helper type

--- monorepo/index.ts
+++ monorepo/index.ts
@@ -1,3 +1,8 @@
 export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T
   ? Pick<T, K> & Partial<Record<Exclude<keyof T, K>, never>>
   : never;
+
+export type ReturnsError<T, E extends Error = Error> = OneOf<{
+  error: E;
+  result: T;
+}>;
