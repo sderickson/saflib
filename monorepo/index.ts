78785fdcdebe5523bf897b035b0d41bb028cdd0f
Scott Erickson <sderickson@gmail.com>
2025-04-22 13:17:39 -0700

    add throwError

--- monorepo/index.ts
+++ monorepo/index.ts
@@ -6,3 +6,23 @@ export type ReturnsError<T, E extends Error = Error> = OneOf<{
   error: E;
   result: T;
 }>;
+
+type NonUndefined<T> = T extends undefined ? never : T;
+
+export const throwError = async <T>(
+  promise: Promise<ReturnsError<T>>,
+  ErrorClass: new (message: string, options?: ErrorOptions) => Error = Error,
+): Promise<NonUndefined<T>> => {
+  /*
+  If a Promise which returns { result, error } is unlikely to error,
+  use this function to throw an error in that unlikely event. It will
+  handle chaining and such.
+
+  Use this function responsibly.
+  */
+  const { result, error } = await promise;
+  if (error) {
+    throw new ErrorClass("Failed to update call log", { cause: error });
+  }
+  return result as NonUndefined<T>; // not sure why TS thinks result might be undefined
+};
