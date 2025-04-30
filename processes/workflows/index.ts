993e580fada1c3ab268113aa2023560729135859
Scott Erickson <sderickson@gmail.com>
2025-04-30 14:33:38 -0700

    clean up spec-project

--- processes/workflows/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ConcreteWorkflow } from "@saflib/workflows";
-import { SpecProjectWorkflow } from "./spec-project.ts";
-
-const workflowClasses: ConcreteWorkflow[] = [SpecProjectWorkflow];
-
-export default workflowClasses;
