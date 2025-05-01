095c42b0a6c8d76da460ecfa0c95d761858d5863
Scott Erickson <sderickson@gmail.com>
2025-05-01 10:40:10 -0700

    Add update-spec workflow

---
+++ openapi-specs/workflows/index.ts
@@ -0,0 +1,6 @@
+import { UpdateSpecWorkflow } from "./update-spec.ts";
+import type { ConcreteWorkflow } from "@saflib/workflows";
+
+const workflowClasses: ConcreteWorkflow[] = [UpdateSpecWorkflow];
+
+export default workflowClasses;
