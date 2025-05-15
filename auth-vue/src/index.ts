5036c98f76e9be92c71c865da349fb1127e54475
Scott Erickson <sderickson@gmail.com>
2025-05-15 09:35:06 -0700

    export rules

--- auth-vue/src/index.ts
+++ auth-vue/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./requests/auth.ts";
 export * from "./requests/types.ts";
+export * from "./utils/rules.ts";
 export { default as LoginPage } from "./components/LoginPage.vue";
 export { default as RegisterPage } from "./components/RegisterPage.vue";
 export { default as ForgotPasswordPage } from "./components/ForgotPasswordPage.vue";
