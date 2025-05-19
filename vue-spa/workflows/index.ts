5b3087531ee7dcde4d3fbbebc741b236aec37ca5
Scott Erickson <sderickson@gmail.com>
2025-05-19 11:22:45 -0700

    create stub workflow

--- vue-spa/workflows/index.ts
+++ vue-spa/workflows/index.ts
@@ -1,6 +1,10 @@
 import { AddSpaWorkflow } from "./add-spa.ts";
+import { AddSpaPageWorkflow } from "./add-spa-page.ts";
 import type { ConcreteWorkflow } from "@saflib/workflows";
 
-const workflowClasses: ConcreteWorkflow[] = [AddSpaWorkflow];
+const workflowClasses: ConcreteWorkflow[] = [
+  AddSpaWorkflow,
+  AddSpaPageWorkflow,
+];
 
 export default workflowClasses;
