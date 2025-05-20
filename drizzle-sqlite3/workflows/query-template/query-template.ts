c5888004ade96f6ba7a6b05c67322cffa5197b09
Scott Erickson <sderickson@gmail.com>
2025-05-20 13:00:49 -0700

    fix

--- drizzle-sqlite3/workflows/query-template/query-template.ts
+++ drizzle-sqlite3/workflows/query-template/query-template.ts
@@ -2,7 +2,7 @@
 import { someDbManager } from "../../instances.ts";
 import { SomeError } from "../../errors.ts";
 import type { SomeDbType } from "../../types.ts";
-import { ReturnsError } from "@saflib/monorepo";
+import type { ReturnsError } from "@saflib/monorepo";
 
 import { queryWrapper } from "@saflib/drizzle-sqlite3";
 import type { DbKey } from "@saflib/drizzle-sqlite3";
