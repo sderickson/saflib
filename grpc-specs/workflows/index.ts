0b98faa0bc7e92dabceb0b4a99ba339aa5c6d4d7
Scott Erickson <sderickson@gmail.com>
2025-05-22 16:38:45 -0700

    add update-grpc-spec workflow

---
+++ grpc-specs/workflows/index.ts
@@ -0,0 +1,6 @@
+import { UpdateGrpcSpecWorkflow } from "./update-grpc-spec.ts";
+import type { ConcreteWorkflow } from "@saflib/workflows";
+
+const workflowClasses: ConcreteWorkflow[] = [UpdateGrpcSpecWorkflow];
+
+export default workflowClasses;
