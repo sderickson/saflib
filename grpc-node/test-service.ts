1d45a21c0295f8adf1f09d52ca488d90a9bfaca3
Scott Erickson <sderickson@gmail.com>
2025-05-23 08:44:25 -0700

    refactor

---
+++ grpc-node/test-service.ts
@@ -0,0 +1,33 @@
+import * as grpc from "@grpc/grpc-js";
+import { startGrpcServer } from "@saflib/grpc-node";
+import { expect, vi } from "vitest";
+let port = Math.floor(Math.random() * 10000) + 50000;
+
+async function runTestServer<S extends grpc.Server>(service: S) {
+  let host: string;
+  while (true) {
+    host = `0.0.0.0:${port}`;
+    const p = startGrpcServer(service, { port });
+    if (vi.isFakeTimers()) {
+      vi.advanceTimersByTime(0);
+    }
+    try {
+      // in case of collisions, try a different port
+      await p;
+      break;
+    } catch (e) {
+      port++;
+    }
+  }
+  return host;
+}
+
+export { runTestServer };
+
+export async function resolveGrpcRequest(p: Promise<any>) {
+  if (vi.isFakeTimers()) {
+    vi.advanceTimersByTime(1000);
+  }
+  const result = await p;
+  expect(result.successful).toBe(true);
+}
