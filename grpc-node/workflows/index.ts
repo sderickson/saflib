710e4b8c7eb0227a8991b34953362d8c6bdd7763
Scott Erickson <sderickson@gmail.com>
2025-05-23 08:29:35 -0700

    add add-grpc-handler stub

---
+++ grpc-node/workflows/index.ts
@@ -0,0 +1,6 @@
+import { AddGrpcHandlerWorkflow } from "./add-grpc-handler.ts";
+import type { ConcreteWorkflow } from "@saflib/workflows";
+
+const workflowClasses: ConcreteWorkflow[] = [AddGrpcHandlerWorkflow];
+
+export default workflowClasses;
