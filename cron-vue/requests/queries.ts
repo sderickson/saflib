e2854c736835b067408a168b7beabbfa7dde3352
Scott Erickson <sderickson@gmail.com>
2025-06-06 09:46:32 -0700

    add cron-vue admin page

---
+++ cron-vue/requests/queries.ts
@@ -0,0 +1,29 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/vue-query";
+import { client } from "./client.js";
+import type { CronRequest, CronResponse } from "@vendata/specs-cron";
+import { TanstackError, handleClientMethod } from "@saflib/vue-spa";
+
+export function useListCronJobs() {
+  return useQuery<CronResponse["listCronJobs"][200], TanstackError>({
+    queryKey: ["cron", "jobs"],
+    queryFn: () => {
+      return handleClientMethod(client.GET("/cron/jobs", {}));
+    },
+  });
+}
+
+export function useUpdateCronJobSettings() {
+  const queryClient = useQueryClient();
+  return useMutation<
+    CronResponse["updateCronJobSettings"][200],
+    TanstackError,
+    CronRequest["updateCronJobSettings"]
+  >({
+    mutationFn: (body) => {
+      return handleClientMethod(client.PUT("/cron/jobs/settings", { body }));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cron", "jobs"] });
+    },
+  });
+}
