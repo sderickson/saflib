c950b4ac289eae18f538e77f7cf34578525e08fa
Scott Erickson <sderickson@gmail.com>
2025-06-16 12:41:29 -0700

    used commander

--- vitest/test-coverage.ts
+++ vitest/test-coverage.ts
@@ -3,9 +3,56 @@
 // Run `TZ=UTC vitest run --coverage` in the current directory
 // and tee the output to a file.
 
-import { exec } from "child_process";
+import { Command } from "commander";
+import { spawn } from "child_process";
+import { writeFile } from "fs/promises";
 
-const coverageFile = "./coverage-log.txt";
-console.log("running coverage...");
+const program = new Command()
+  .name("saf-test-coverage")
+  .description("Wrapper around vitest coverage")
+  .option("-c, --capture-log", "Capture the log output - but TTY is lost!")
+  .action(async (options) => {
+    const captureLog = options.captureLog;
+    const coverageFile = "./coverage/coverage-log.txt";
 
-exec(`TZ=UTC vitest run --coverage | tee ${coverageFile}`);
+    const child = spawn("vitest", ["run", "--coverage"], {
+      env: {
+        ...process.env,
+        TZ: "UTC",
+      },
+      stdio: ["inherit", captureLog ? "pipe" : "inherit", "inherit"],
+    });
+
+    let stdout = "";
+    if (captureLog && child.stdout) {
+      child.stdout.pipe(process.stdout);
+      process.stdout.on("data", (data) => {
+        console.log("GOT DATA", data);
+        const chunk = data.toString();
+        stdout += chunk;
+      });
+    }
+
+    await new Promise<void>((resolve, reject) => {
+      child.on("close", async (code) => {
+        if (code === 0) {
+          try {
+            if (captureLog) {
+              await writeFile(coverageFile, stdout);
+            }
+            resolve();
+          } catch (err) {
+            reject(err);
+          }
+        } else {
+          reject(new Error(`Process exited with code ${code}`));
+        }
+      });
+
+      child.on("error", reject);
+    });
+
+    process.exit(0);
+  });
+
+program.parse(process.argv);
