0f92b853f40b4e7e8110180a408fa2bb9c0e06e8
Scott Erickson <sderickson@gmail.com>
2025-06-24 11:27:32 -0700

    fix

--- email-node/routes/emails/get-sent-emails.ts
+++ email-node/routes/emails/get-sent-emails.ts
@@ -1,11 +1,67 @@
 import { createHandler } from "@saflib/express";
 import createError from "http-errors";
-import { mockingOn, sentEmails } from "../../client/email-client.ts";
+import {
+  EmailOptions,
+  mockingOn,
+  sentEmails,
+} from "../../client/email-client.ts";
+import { EmailResponse, SentEmail } from "@saflib/email-spec";
 
 export const getSentEmails = createHandler(async (_req, res) => {
   if (!mockingOn) {
     throw createError(403, "Forbidden - server is not mocking email sends");
   }
 
-  res.status(200).json(sentEmails);
+  res
+    .status(200)
+    .json(
+      sentEmails.map(
+        convertEmailOptionsToApiResponse,
+      ) satisfies EmailResponse["listSentEmails"][200],
+    );
 });
+
+const convertEmailOptionsToApiResponse = (
+  emailOptions: EmailOptions,
+): SentEmail => {
+  return {
+    from: convertEmailFieldToString(emailOptions.from)[0],
+    to: convertEmailFieldToString(emailOptions.to),
+    cc: convertEmailFieldToString(emailOptions.cc),
+    bcc: convertEmailFieldToString(emailOptions.bcc),
+    subject: emailOptions.subject ?? "<no subject>",
+    text: convertTextFieldToString(emailOptions.text),
+    html: convertTextFieldToString(emailOptions.html),
+  };
+};
+
+const convertEmailFieldToString = (
+  emailField:
+    | EmailOptions["to"]
+    | EmailOptions["cc"]
+    | EmailOptions["bcc"]
+    | EmailOptions["from"],
+): string[] => {
+  if (Array.isArray(emailField)) {
+    return emailField.map(convertEmailFieldToString).flat();
+  }
+  if (typeof emailField === "string") {
+    return [emailField];
+  }
+  if (!emailField) {
+    return [];
+  }
+  return [`${emailField.name} <${emailField.address}>`];
+};
+
+const convertTextFieldToString = (
+  textField: EmailOptions["text"] | EmailOptions["html"],
+): string => {
+  if (typeof textField === "string") {
+    return textField;
+  }
+  if (!textField) {
+    return "<no text>";
+  }
+  return textField.toString();
+};
--- email-node/routes/emails/index.ts
+++ email-node/routes/emails/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { getSentEmails } from "./get-sent-emails.ts";
+import { createPreMiddleware } from "@saflib/express";
+import { jsonSpec } from "@saflib/email-spec";
 
 export function createEmailsRouter() {
   const router = express.Router();
 
+  const preMiddleware = createPreMiddleware({
+    apiSpec: jsonSpec,
+    authRequired: false,
+  });
+
+  router.use("/email", preMiddleware);
   router.get("/email/sent", getSentEmails);
 
   return router;
