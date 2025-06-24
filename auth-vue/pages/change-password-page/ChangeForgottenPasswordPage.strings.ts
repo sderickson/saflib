dc25cc3b0a7f90656a8a7f2cc42a3f53c49c480a
Scott Erickson <sderickson@gmail.com>
2025-06-24 16:21:26 -0700

    update change password page

---
+++ auth-vue/pages/change-password-page/ChangeForgottenPasswordPage.strings.ts
@@ -0,0 +1,15 @@
+export const change_forgotten_password_page = {
+  reset_password: "Reset Password",
+  please_set_a_new_password: "Please set a new password.",
+  please_confirm_your_password: "Please confirm your password.",
+  passwords_do_not_match: "Passwords do not match.",
+  new_password: {
+    label: "New Password",
+  },
+  confirm_password: {
+    label: "Confirm New Password",
+  },
+  reset_password_button: "Reset Password",
+  resetting: "Resetting...",
+  continue_to_login: "Continue to Login",
+};
--- auth-vue/pages/change-password-page/ChangeForgottenPasswordPage.vue
+++ auth-vue/pages/change-password-page/ChangeForgottenPasswordPage.vue
@@ -1,8 +1,84 @@
+<template>
+  <div>
+    <v-alert v-if="successMessage" type="success" variant="tonal" class="mb-4">
+      {{ successMessage }}
+    </v-alert>
+
+    <v-alert v-if="errorMessage" type="error" variant="tonal" class="mb-4">
+      {{ errorMessage }}
+    </v-alert>
+
+    <v-form v-if="!successMessage" v-model="valid">
+      <div class="mb-4 font-weight-medium">Reset Password</div>
+
+      <div class="text-body-2 text-medium-emphasis mb-4">
+        Please set a new password.
+      </div>
+
+      <v-text-field
+        v-model="newPassword"
+        v-bind="change_forgotten_password_page.new_password"
+        prepend-inner-icon="mdi-lock-outline"
+        type="password"
+        :rules="passwordRules"
+        :disabled="isPending"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="confirmPassword"
+        v-bind="change_forgotten_password_page.confirm_password"
+        prepend-inner-icon="mdi-lock-check-outline"
+        type="password"
+        :rules="[
+          (v) =>
+            !!v || change_forgotten_password_page.please_confirm_your_password,
+          (_) =>
+            passwordsMatch ||
+            change_forgotten_password_page.passwords_do_not_match,
+        ]"
+        :disabled="isPending"
+      ></v-text-field>
+
+      <v-btn
+        class="my-5"
+        color="primary"
+        size="large"
+        variant="tonal"
+        block
+        :disabled="!valid || !passwordsMatch || isPending"
+        :loading="isPending"
+        @click="handleSubmit"
+      >
+        {{
+          isPending
+            ? change_forgotten_password_page.resetting
+            : change_forgotten_password_page.reset_password
+        }}
+      </v-btn>
+    </v-form>
+
+    <v-card-text v-else class="text-center">
+      <router-link class="text-blue text-decoration-none" to="/login">
+        {{ change_forgotten_password_page.continue_to_login }}
+        <v-icon icon="mdi-chevron-right"></v-icon>
+      </router-link>
+    </v-card-text>
+  </div>
+</template>
+
+<style scoped>
+.pointer-events-none {
+  pointer-events: none;
+  opacity: 0.7;
+}
+</style>
+
 <script setup lang="ts">
 import { ref, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useResetPassword } from "../../src/requests/auth";
 import { passwordRules } from "../../src/utils/rules";
+import { change_forgotten_password_page } from "./ChangeForgottenPasswordPage.strings";
 
 const route = useRoute();
 const token = route.query.token as string;
@@ -38,91 +114,3 @@ const handleSubmit = async () => {
   }
 };
 </script>
-
-<template>
-  <div class="d-flex justify-center align-center flex-column fill-height">
-    <v-card class="mx-auto pa-12 pb-8" elevation="8" width="448" rounded="lg">
-      <v-alert
-        v-if="successMessage"
-        type="success"
-        variant="tonal"
-        class="mb-4"
-      >
-        {{ successMessage }}
-      </v-alert>
-
-      <v-alert v-if="errorMessage" type="error" variant="tonal" class="mb-4">
-        {{ errorMessage }}
-      </v-alert>
-
-      <v-form v-if="!successMessage" v-model="valid">
-        <div class="text-subtitle-1 text-medium-emphasis">Reset Password</div>
-
-        <div class="text-body-2 text-medium-emphasis mb-4">
-          Please set a new password.
-        </div>
-
-        <v-text-field
-          v-model="newPassword"
-          density="compact"
-          placeholder="New Password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          type="password"
-          :rules="passwordRules"
-          :disabled="isPending"
-        ></v-text-field>
-
-        <v-text-field
-          v-model="confirmPassword"
-          density="compact"
-          placeholder="Confirm New Password"
-          prepend-inner-icon="mdi-lock-check-outline"
-          variant="outlined"
-          type="password"
-          :rules="[
-            (v) => !!v || 'Please confirm your password',
-            (_) => passwordsMatch || 'Passwords do not match',
-          ]"
-          :disabled="isPending"
-        ></v-text-field>
-
-        <v-btn
-          class="my-5"
-          color="blue"
-          size="large"
-          variant="tonal"
-          block
-          :disabled="!valid || !passwordsMatch || isPending"
-          :loading="isPending"
-          @click="handleSubmit"
-        >
-          {{ isPending ? "Resetting..." : "Reset Password" }}
-        </v-btn>
-
-        <v-card-text class="text-center">
-          <router-link
-            class="text-blue text-decoration-none"
-            to="/login"
-            :class="{ 'pointer-events-none': isPending }"
-          >
-            Back to Login <v-icon icon="mdi-chevron-right"></v-icon>
-          </router-link>
-        </v-card-text>
-      </v-form>
-
-      <v-card-text v-else class="text-center">
-        <router-link class="text-blue text-decoration-none" to="/login">
-          Continue to Login <v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
-    </v-card>
-  </div>
-</template>
-
-<style scoped>
-.pointer-events-none {
-  pointer-events: none;
-  opacity: 0.7;
-}
-</style>
