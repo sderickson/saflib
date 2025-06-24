7f96e51d94945adbbaa4087b01e655ae37b0c93a
Scott Erickson <sderickson@gmail.com>
2025-06-24 13:55:42 -0700

    fix

--- express/src/middleware/blockHtml.ts
+++ express/src/middleware/blockHtml.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 
 export const blockHtml = (req: Request, res: Response, next: NextFunction) => {
   const { body } = req;
