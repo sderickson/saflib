3df788375819d48a374e2d394d536d8cf2c91477
Scott Erickson <sderickson@gmail.com>
2025-06-24 16:10:59 -0700

    continue

---
+++ auth-vue/pages/verify-email/VerifyEmailPage.strings.ts
@@ -0,0 +1,12 @@
+export const verify_email_page = {
+  verify_email: "Verify Email",
+  verify_email_sent:
+    "Verification email sent. Please check your email for the verification link.",
+  verify_email_success: "You have successfully verified your email address.",
+  continue_to_app: "Continue to App",
+  not_logged_in: "You are not logged in.",
+  not_authorized: "You are not authorized to verify this email.",
+  unknown_error: "Unknown error. Please try again.",
+  sending: "Sending...",
+  resend_verification_email: "Resend Verification Email",
+};
