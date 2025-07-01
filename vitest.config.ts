86ff13d65424f6eb61a7ad770dfe8fe45d2a40b3
Scott Erickson <sderickson@gmail.com>
2025-07-01 15:16:10 -0700

    add global test

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    env: {
+      TZ: "UTC",
+      NODE_OPTIONS: "--disable-warning=DEP0040",
+    },
+
+    projects: ["*/vitest.config.{ts,js,mts,mjs}"],
+  },
+});
