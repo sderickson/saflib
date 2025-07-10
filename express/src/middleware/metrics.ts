a9907690530d7802220288f2dde50004556955a6
Scott Erickson <sderickson@gmail.com>
2025-07-10 10:53:43 -0700

    Add RED metrics to express servers (#76)
    Starting to work on getting metrics gathered on the server side.
    
    I'm starting with getting metrics for all the API routes. I decided to
    go with `express-prom-bundle` because it's at the order of complexity,
    and provides exactly the metrics I want for my application (RED). I
    started out trying `@opentelemetry/instrumentation-express` but I'm not
    currently interested in setting up traces, and it doesn't provide the
    metrics I want as near as I can tell. Also generally the open telemetry
    site seems to be geared towards large infras and that's just not the
    scope I'm aiming for right now (though I am designing things to readily
    scale up if needed). I'll adopt things like centralized metric
    collectors once there's a clear need for them.
    
    I'm already seeing bot traffic polluting these metrics. I'll need to fix
    that.

---
+++ express/src/middleware/metrics.ts
@@ -0,0 +1,8 @@
+import promBundle from "express-prom-bundle";
+
+export const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  includeUp: true,
+});
