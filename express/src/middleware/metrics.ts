613d33b565095401be6b88371b45ff078e86c26d
Scott Erickson <sderickson@gmail.com>
2025-07-10 16:54:05 -0700

    filter out script kiddies

--- express/src/middleware/metrics.ts
+++ express/src/middleware/metrics.ts
@@ -5,4 +5,13 @@ export const metricsMiddleware = promBundle({
   includePath: true,
   includeStatusCode: true,
   includeUp: true,
+  transformLabels: (labels, req, res) => {
+    // For 404s
+    if (res.statusCode === 404 && !req.openapi) {
+      // apparently you don't return a new labels object... interesting
+      labels.path = "/global-404";
+      return labels;
+    }
+    return labels;
+  },
 });
