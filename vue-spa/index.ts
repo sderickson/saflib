0caec08708a22503000b83864900d48bc763ffd1
Scott Erickson <sderickson@gmail.com>
2025-07-30 13:14:04 -0700

    add note

--- vue-spa/index.ts
+++ vue-spa/index.ts
@@ -45,6 +45,7 @@ export const createVueApp = (
   return createApp(app);
 };
 
+// Import this here so that typescript is okay with accessing import.meta.env
 // @ts-expect-error - vite/client is not a module
 import type { ImportMetaEnv as _ImportMetaEnv } from "vite/client";
 export const getViteEnv = () => {
