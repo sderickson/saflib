a5346ecf24ba753e6813c6a365a2d899513a75c7
Scott Erickson <sderickson@gmail.com>
2025-08-18 14:51:04 -0700

    2025 08 18 fixes (#98)
    Following #97, I went to my other repo to propagate changes, and I had
    to fix some things as a result.
    
    * Continue to export types from dev-tools, I need those for pages which
    use the static assets, in particular the manifest type.
    * Got rid of dotenv. I realized I haven't been using it in a while, and
    all it does is log advertisements for dotenvx. Deleted.
    * Export `makeUserHeaders`. Forgot to do that.

--- express/src/index.ts
+++ express/src/index.ts
@@ -6,13 +6,6 @@
 
 export { startExpressServer, type StartServerOptions } from "./bin/www.ts";
 
-// consumers of this library automatically get env variables
-import dotenv from "dotenv";
-if (process.env.NODE_ENV !== "test") {
-  // hush warning - TODO: better handle env anyway
-  dotenv.config();
-}
-
 // middleware bundles
 export {
   createGlobalMiddleware,
@@ -24,3 +17,5 @@ export {
 
 // route handler utilities
 export { createHandler } from "./handler.ts";
+
+export { makeUserHeaders } from "./vitest-helpers.ts";
