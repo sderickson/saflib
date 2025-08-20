45d4362bf6623373663d6f3e9394bbee9eafd08e
Scott Erickson <sderickson@gmail.com>
2025-08-20 16:39:41 -0700

    rename auth-vue to auth

--- dev-tools/src/monorepo.mock.ts
+++ dev-tools/src/monorepo.mock.ts
@@ -35,7 +35,7 @@ export const monorepoPackageMock = {
     name: "@foo/auth-web-client",
     dependencies: {
       "@saflib/vue-spa": "*",
-      "@saflib/auth-vue": "*",
+      "@saflib/auth": "*",
     },
   }),
 
@@ -86,7 +86,7 @@ export const monorepoPackageMock = {
     },
   }),
   "/app/saflib/auth-vue/package.json": JSON.stringify({
-    name: "@saflib/auth-vue",
+    name: "@saflib/auth",
     dependencies: {
       "third-party-lib": "3.2.1",
       "@saflib/vue-spa": "*",
