f80b032a198a3291848590b00b102e58e1bf411f
Scott Erickson <sderickson@gmail.com>
2025-08-25 10:59:42 -0700

    let's try this again

--- commander/workflows/index.ts
+++
@@ -1,6 +0,0 @@
-import { AddCommandWorkflow } from "./add-command.ts";
-import type { ConcreteWorkflow } from "@saflib/workflows";
-
-const workflowClasses: ConcreteWorkflow[] = [AddCommandWorkflow];
-
-export default workflowClasses;
