11f7360dc4aafea413fc967ede820d0c08edaf62
Scott Erickson <sderickson@gmail.com>
2025-08-25 10:41:13 -0700

    initial workflow creation

---
+++ commander/workflows/index.ts
@@ -0,0 +1,6 @@
+import { AddCommandWorkflow } from "./add-command.ts";
+import type { ConcreteWorkflow } from "@saflib/workflows";
+
+const workflowClasses: ConcreteWorkflow[] = [AddCommandWorkflow];
+
+export default workflowClasses;
