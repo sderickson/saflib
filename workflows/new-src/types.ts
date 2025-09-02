0955a25c86e553dc193b357565a78d904e2d5ff7
Scott Erickson <sderickson@gmail.com>
2025-09-02 13:38:31 -0700

    much better

--- workflows/new-src/types.ts
+++ workflows/new-src/types.ts
@@ -1,34 +1,33 @@
-import { createActor, setup } from "xstate";
-const machine = setup({
-  types: {
-    input: {} as { foo: string },
-    context: {} as { bar: string },
-  },
-}).createMachine({
-  id: "foo",
-  context: ({ input }) => ({
-    bar: input.foo,
-  }),
-  initial: "bar",
-  states: {
-    bar: {
-      on: {
-        baz: "baz",
-      },
-    },
-  },
-});
+import {
+  //  setup,
+  type AnyStateMachine,
+  type InputFrom,
+} from "xstate";
 
-interface Context {
-  baz: string;
-}
+// const machine = setup({
+//   types: {
+//     input: {} as { foo: string },
+//     context: {} as { bar: string },
+//   },
+// }).createMachine({
+//   id: "foo",
+//   context: ({ input }) => ({
+//     bar: input.foo,
+//   }),
+//   initial: "bar",
+//   states: {
+//     bar: {
+//       on: {
+//         baz: "baz",
+//       },
+//     },
+//   },
+// });
 
-const step: Step<typeof machine> = {
-  actor: machine,
-  input: { foo: "bar" },
-};
-
-const actor = createActor(machine, { input: { foo: "bar" } });
+// const step: Step<typeof machine> = {
+//   machine: machine,
+//   input: { foo: "bar" },
+// };
 
 /**
  * Required argument for the workflow, in a format the CLI tool (commander) can use.
@@ -44,34 +43,11 @@ export interface CLIArgument {
 }
 
 /**
- * Helper type to extract the input type from different types of actors
- */
-type ActorInput<T> =
-  T extends Promise<infer U>
-    ? U
-    : T extends { input: infer I }
-      ? I
-      : T extends { send: (event: infer E) => void }
-        ? E
-        : T extends { getSnapshot: () => infer S }
-          ? S
-          : any;
-
-/**
- * Common types of actors that can be used in workflows
- */
-type WorkflowActor =
-  | Promise<any>
-  | { input: any; send: (event: any) => void; getSnapshot: () => any }
-  | { send: (event: any) => void; getSnapshot: () => any }
-  | { getSnapshot: () => any };
-
-/**
  * A step in a workflow with an actor and its corresponding input
  */
-type Step<Actor extends WorkflowActor> = {
-  actor: Actor;
-  input: ActorInput<Actor>;
+type Step<Machine extends AnyStateMachine> = {
+  machine: Machine;
+  input: InputFrom<Machine>;
 };
 
 /**
@@ -93,5 +69,5 @@ export interface Workflow<C extends Record<string, any>> {
    */
   docFiles: Record<string, string>;
 
-  steps: Array<Step<WorkflowActor>>;
+  steps: Array<Step<AnyStateMachine>>;
 }
