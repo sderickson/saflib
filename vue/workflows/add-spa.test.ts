e3a8d4919a85cc6abffe3779935c45b0571c85a0
Scott Erickson <sderickson@gmail.com>
2025-09-05 07:32:36 -0700

    simplify a test

--- vue/workflows/add-spa.test.ts
+++ vue/workflows/add-spa.test.ts
@@ -1,34 +1,9 @@
-import { describe, it, expect } from "vitest";
-import { AddSpaWorkflowMachine } from "./add-spa.ts";
-import { createActor, waitFor } from "xstate";
-import {
-  workflowAllSettled,
-  continueWorkflow,
-} from "../../workflows/core/utils.ts";
+import { describe, it } from "vitest";
+import { AddSpaWorkflow } from "./add-spa.ts";
 import { dryRunWorkflow } from "../../workflows/saf-workflow-cli/utils.ts";
 
 describe("add-spa", () => {
-  it("should create a new SPA", async () => {
-    const workflow = await dryRunWorkflow(AddSpaWorkflowMachine);
-    const checklist = workflow.checklist;
-    const actor = createActor(AddSpaWorkflowMachine, {
-      input: {
-        name: "test-spa",
-        dryRun: true,
-      },
-    });
-    actor.start();
-    await waitFor(actor, workflowAllSettled);
-    let lastStateName = "";
-    while (actor.getSnapshot().status !== "done") {
-      continueWorkflow(actor);
-      await waitFor(actor, workflowAllSettled);
-      const currentStateName = actor.getSnapshot().value;
-      if (currentStateName === lastStateName) {
-        throw new Error(`Workflow is stuck on state ${currentStateName}.`);
-      }
-      lastStateName = currentStateName;
-    }
-    expect(actor.getSnapshot().status).toBe("done");
+  it("should successfully dry run", async () => {
+    await dryRunWorkflow(AddSpaWorkflow);
   });
 });
