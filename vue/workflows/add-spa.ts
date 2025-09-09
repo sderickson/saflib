35d9b28e643629b5eb049467f92955dc4f134f8d
Scott Erickson <sderickson@gmail.com>
2025-09-08 18:35:45 -0700

    remove temporary test

--- vue/workflows/add-spa.ts
+++ vue/workflows/add-spa.ts
@@ -6,9 +6,6 @@ import {
 } from "@saflib/workflows";
 import path from "node:path";
 import { readFileSync } from "node:fs";
-import { AddWorkflowDefinition, makeWorkflowMachine } from "@saflib/workflows";
-
-const AddWorkflowMachine = makeWorkflowMachine(AddWorkflowDefinition);
 
 const sourceDir = path.join(import.meta.dirname, "spa-template");
 
@@ -76,11 +73,6 @@ export const AddSpaWorkflowDefinition = defineWorkflow<
     step(CopyStepMachine, ({ context }) => ({
       name: context.name,
       targetDir: context.targetDir,
-    })),
-
-    step(AddWorkflowMachine, () => ({
-      name: "another-name-to-copy",
-      nestChecklist: "Add the new Workflow to the monorepo for some reason...",
     })),
 
     step(PromptStepMachine, ({ context }) => ({
--- workflows/index.ts
+++ workflows/index.ts
@@ -1,3 +1,2 @@
 export * from "./saf-workflow-cli/index.ts";
 export * from "./core/index.ts";
-export * from "./workflows/add-workflow.ts";
