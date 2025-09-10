d619ebcb30c65ebafb625e9554aa6856a1c98892
Scott Erickson <sderickson@gmail.com>
2025-09-10 15:06:27 -0700

    update

--- workflows/workflows/templates/index.ts
+++ workflows/workflows/templates/index.ts
@@ -1,8 +1,13 @@
 import { TemplateFileWorkflowDefinition } from "./template-file.ts";
 import type { WorkflowDefinition } from "@saflib/workflows";
 
+export {
+  // Export each workflow definition separately
+  TemplateFileWorkflowDefinition,
+};
+
 const workflowDefinitions: WorkflowDefinition[] = [
+  // And have the default export be the array of all of them
   TemplateFileWorkflowDefinition,
 ];
-
 export default workflowDefinitions;
