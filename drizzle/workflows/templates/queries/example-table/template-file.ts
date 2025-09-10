25069a7ffdabdbcb6529feea57e571e74e9f6379
Scott Erickson <sderickson@gmail.com>
2025-09-09 19:43:13 -0700

    remove a few ..'s

--- drizzle/workflows/templates/queries/example-table/template-file.ts
+++ drizzle/workflows/templates/queries/example-table/template-file.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck - TODO remove this line as part of workflow
-import { someDbManager } from "../../../../instances.ts";
-import { SomeError } from "../../../../errors.ts";
-import type { SomeDbType } from "../../../../types.ts";
+import { templateFileManager } from "../../instances.ts";
+import { SomeError } from "../../errors.ts";
+import type { SomeDbType } from "../../types.ts";
 import type { ReturnsError } from "@saflib/monorepo";
 
 import { queryWrapper } from "@saflib/drizzle";
@@ -12,9 +12,9 @@ export type TemplateFileError = SomeError;
 export const templateFile = queryWrapper(
   async (
     dbKey: DbKey,
-    params: SomeDbType,
+    params: SomeDbType
   ): Promise<ReturnsError<SomeDbType, TemplateFileError>> => {
     const db = someDbManager.get(dbKey);
     return { result: {} };
-  },
+  }
 );
