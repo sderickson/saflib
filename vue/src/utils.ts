c8ee3d00bb6f705721bf0344ba66395428f1c0e7
Scott Erickson <sderickson@gmail.com>
2025-09-12 08:14:51 -0700

    Use getClientName to derive subdomain

--- vue/src/utils.ts
+++ vue/src/utils.ts
@@ -53,12 +53,16 @@ export const getClientName = () => {
  * What is returned is based on the current domain; if the link is to the same subdomain, this returns a "to" prop,
  * otherwise it returns an "href" prop. That way a link will use vue-router wherever possible, to avoid full page
  * reloads.
+ *
+ * The current domain is derived from the client name, which is also the subdomain.
  */
 export const linkToProps = (link: Link) => {
-  const currentSubdomain = document.location.hostname
-    .split(".")
-    .slice(0, -2)
-    .join(".");
+  const currentSubdomain = getClientName();
+  if (!currentSubdomain) {
+    throw new Error(
+      "You must call setClientName with your subdomain before using linkToProps",
+    );
+  }
   if (currentSubdomain === link.subdomain) {
     return {
       to: link.path,
