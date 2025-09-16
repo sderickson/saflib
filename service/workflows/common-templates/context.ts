8ed981b2031b160b6bc2a8177791d7e232797f0d
Scott Erickson <sderickson@gmail.com>
2025-09-16 11:51:29 -0700

    small fix

--- service/workflows/common-templates/context.ts
+++ service/workflows/common-templates/context.ts
@@ -1,6 +1,6 @@
 import { AsyncLocalStorage } from "async_hooks";
 import type { DbKey } from "@saflib/drizzle";
-// import { templateFileDb } from "@your-org/template-file-db";
+import { templateFileDb } from "@template/file-db";
 
 export interface TemplateFileServiceContext {
   templateFileDbKey: DbKey;
@@ -14,11 +14,11 @@ export interface TemplateFileServiceContextOptions {
 }
 
 // TODO: Uncomment this and make sure it's correct
-// export const makeContext = (
-//   options: MakeContextOptions = {},
-// ): ApiServiceContext => {
-//   const dbKey = options.templateFileDbKey ?? templateFileDb.connect();
-//   return {
-//     templateFileDbKey: dbKey,
-//   };
-// };
+export const makeContext = (
+  options: TemplateFileServiceContextOptions = {},
+): TemplateFileServiceContext => {
+  const dbKey = options.templateFileDbKey ?? templateFileDb.connect();
+  return {
+    templateFileDbKey: dbKey,
+  };
+};
