bc87ebe8d3d0a26d39855fbc823e91ee1298d0ba
Scott Erickson <sderickson@gmail.com>
2025-09-16 13:25:21 -0700

    some dep fixes

--- service/workflows/service-templates/run.ts
+++ service/workflows/service-templates/run.ts
@@ -12,6 +12,7 @@ import { setServiceName } from "@saflib/node";
 import { validateEnv } from "@saflib/env";
 import envSchema from "./env.schema.combined.json" with { type: "json" };
 import { typedEnv } from "./env.ts";
+import { makeContext } from "@template/file-service-common";
 
 validateEnv(process.env, envSchema);
 setServiceName("template-file");
@@ -24,10 +25,11 @@ async function main() {
     log.info("Starting up TemplateFile service...");
     log.info("Connecting to template-file-db...");
     const dbKey = templateFileDb.connect({ onDisk: true, doNotCreate: true });
+    const context = makeContext({ templateFileDbKey: dbKey });
     log.info("template-file-db connection complete.");
 
     log.info("Starting template-file-http...");
-    const expressApp = createTemplateFileHttpApp({ templateFileDbKey: dbKey });
+    const expressApp = createTemplateFileHttpApp(context);
     startExpressServer(expressApp, {
       port: parseInt(typedEnv.TEMPLATE_FILE_SERVICE_HTTP_PORT || "3000", 10),
     });
