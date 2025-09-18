4e400537e68e1b369a54da8a9036da2e76ee02e8
Scott Erickson <sderickson@gmail.com>
2025-09-18 11:02:37 -0700

    one more cleanup

--- secrets/secrets-db/index.ts
+++ secrets/secrets-db/index.ts
@@ -2,9 +2,7 @@ export type * from "./types.ts";
 export * from "./errors.ts";
 
 import { secretsDbManager } from "./instances.ts";
-import { secrets } from "./queries/secrets/index.ts";
 
 export const secretsDb = {
   ...secretsDbManager.publicInterface(),
-  secrets,
 };
