ce80cb8aa74837d9c4c887e266d9e5827a8083df
Scott Erickson <sderickson@gmail.com>
2025-09-18 14:25:21 -0700

    minor fix

--- drizzle/workflows/templates/index.ts
+++ drizzle/workflows/templates/index.ts
@@ -5,9 +5,4 @@ import { templateFileDbManager } from "./instances.ts";
 // TODO: Import query modules as you create them
 // import * as templateFiles from "./queries/template-files/index.ts";
 
-export const templateFileDb = {
-  ...templateFileDbManager.publicInterface(),
-  // TODO: Add query modules as you create them
-  // TODO: Remove todos once any queries are added
-  // templateFiles,
-};
+export const templateFileDb = templateFileDbManager.publicInterface();
