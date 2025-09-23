beb0dbf432be38dfe5c71c11f64efcb12e2ed654
Scott Erickson <sderickson@gmail.com>
2025-09-23 12:39:23 -0700

    start over with add display

--- sdk/workflows/index.ts
+++ sdk/workflows/index.ts
@@ -1,17 +1,11 @@
 import { SdkInitWorkflowDefinition } from "./init.ts";
 import { AddQueryWorkflowDefinition } from "./add-query.ts";
-import { AddDisplayWorkflowDefinition } from "./add-display.ts";
 import type { WorkflowDefinition } from "@saflib/workflows";
 
-export {
-  SdkInitWorkflowDefinition,
-  AddQueryWorkflowDefinition,
-  AddDisplayWorkflowDefinition,
-};
+export { SdkInitWorkflowDefinition, AddQueryWorkflowDefinition };
 
 const workflowDefinitions: WorkflowDefinition[] = [
   SdkInitWorkflowDefinition,
   AddQueryWorkflowDefinition,
-  AddDisplayWorkflowDefinition,
 ];
 export default workflowDefinitions;
