1ff1b33cf88b79dfa4eeb468aa878b9840053a6d
Scott Erickson <sderickson@gmail.com>
2025-09-25 12:01:29 -0700

    uncomment

--- dev-tools/src/saf-docker-cli.ts
+++ dev-tools/src/saf-docker-cli.ts
@@ -5,25 +5,23 @@ import { generateDockerfiles } from "./docker.ts";
 import { buildMonorepoContext } from "./workspace.ts";
 import { setupContext } from "@saflib/commander";
 
-console.log("Test");
+const program = new Command()
+  .name("saf-docker")
+  .description("Helps manage Docker-related files in SAF packages.");
 
-// const program = new Command()
-//   .name("saf-docker")
-//   .description("Helps manage Docker-related files in SAF packages.");
+program
+  .command("generate")
+  .description("Generate all Dockerfiles from templates across the monorepo.")
+  .action(() => {
+    console.log("Generating context...");
+    const monorepoContext = buildMonorepoContext();
+    console.log("Generating Dockerfiles...");
+    generateDockerfiles(monorepoContext, true);
+    console.log("Done.");
+  });
 
-// program
-//   .command("generate")
-//   .description("Generate all Dockerfiles from templates across the monorepo.")
-//   .action(() => {
-//     console.log("Generating context...");
-//     const monorepoContext = buildMonorepoContext();
-//     console.log("Generating Dockerfiles...");
-//     generateDockerfiles(monorepoContext, true);
-//     console.log("Done.");
-//   });
-
-// console.log("Parsing...");
-// setupContext({ serviceName: "saf-docker" }, () => {
-//   program.parse(process.argv);
-// });
-// console.log("Done done.");
+console.log("Parsing...");
+setupContext({ serviceName: "saf-docker" }, () => {
+  program.parse(process.argv);
+});
+console.log("Done done.");
