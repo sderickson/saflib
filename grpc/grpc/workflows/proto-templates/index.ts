9612f9c436a0800942637f5faad0dbe166865c4a
Scott Erickson <sderickson@gmail.com>
2025-09-26 10:13:25 -0700

    fix

--- grpc/grpc/workflows/proto-templates/index.ts
+++ grpc/grpc/workflows/proto-templates/index.ts
@@ -1,7 +1,7 @@
 export * from "./dist/health/index.ts";
 export * from "./dist/health/get-health.ts";
-export * from "./dist/__group_name__/index.ts";
-export * from "./dist/__group_name__/__target_name__.ts";
+export * from "./dist/__group_name__/index.ts"; // DELETE_THIS_LINE
+export * from "./dist/__group_name__/__target_name__.ts"; // DELETE_THIS_LINE
 export * from "./dist/google/protobuf/timestamp.ts";
 
 import { typedEnv } from "./env.ts";
