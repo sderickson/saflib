b17b95a58c80c43b8791edda1b8a939302c9164b
Scott Erickson <sderickson@gmail.com>
2025-09-30 10:38:04 -0700

    tweak add-spa

--- vue/workflows/template/main.ts
+++ vue/workflows/template/main.ts
@@ -1,8 +1,14 @@
 import { createVueApp, setClientName } from "@saflib/vue";
 import App from "./__ServiceName__App.vue";
 import { router } from "./router.ts";
+import { __service_name___strings } from "./strings.ts";
 
 export const main = () => {
   setClientName("__service_name__");
-  createVueApp(App, { router });
+  createVueApp(App, {
+    router,
+    i18nMessages: {
+      ...__service_name___strings,
+    },
+  });
 };
