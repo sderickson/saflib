edb00090d174549b7153106e496a5e0fb8ddd68f
Scott Erickson <sderickson@gmail.com>
2025-10-01 09:57:16 -0700

    fix

--- workflows/core/xstate-actions/prompt.ts
+++ workflows/core/xstate-actions/prompt.ts
@@ -5,6 +5,7 @@ import {
   type WorkflowContext,
   type WorkflowActionFunction,
 } from "../types.ts";
+import { spawn } from "node:child_process";
 
 /**
  * Action builder for prompting the agent.
@@ -64,14 +65,45 @@ export const printPrompt = ({ msg, context }: PromptParam) => {
   return {};
 };
 
-export const executePrompt = ({ msg, context }: PromptParam) => {
+export const executePrompt = async ({ msg, context }: PromptParam) => {
   if (process.env.NODE_ENV === "test") {
     return { shouldContinue: true };
   }
+  let resolve: (value: unknown) => void;
+  const p = new Promise((r) => {
+    resolve = r;
+  });
+  // const agent = spawn("cursor-agent", [
+  //   "-p",
+  //   '"Say hi"',
+  //   // msg.replace('"', '\\"'),
+  //   "--output-format",
+  //   "stream-json",
+  // ]);
+  const agent = spawn("echo", ["hi1\nhi2\nhi3\nhi4"]);
+  let buffer = "";
+  agent.stdout.on("data", (data) => {
+    const s = data.toString();
+    // console.log("agent stdout", s);
+    buffer += s;
+    while (buffer.includes("\n")) {
+      const line = buffer.split("\n")[0];
+      buffer = buffer.slice(line.length + 1);
+      console.log("\nagent stdout line", line, "\n");
+    }
+  });
+  agent.stderr.on("data", (data) => {
+    console.log("agent stderr", data);
+  });
+  agent.on("close", (code) => {
+    console.log("agent closed with code", code);
+    resolve({ code });
+  });
   console.log(
     "executing prompt",
     msg,
     "with agent config",
     context.agentConfig,
   );
+  return p;
 };
