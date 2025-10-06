0b85670d4801b480555f8cb9ec7915c26447edc5
Scott Erickson <sderickson@gmail.com>
2025-10-06 16:14:03 -0700

    fix an error import issue

--- openapi/index.ts
+++ openapi/index.ts
@@ -1,12 +1,1 @@
-import { readFileSync } from "fs";
-import path from "path";
-
-/**
- * The raw error.yaml file contents from this package. There's not a great way to share OpenAPI schemas between packages, so this is exported for use elsewhere. Mainly this is used by `npm exec saf-specs generate` which writes the contents to `schemas/error.yaml` of whatever package it's run in.
- */
-export const errorSchema = readFileSync(
-  path.join(import.meta.dirname, "./schemas/error.yaml"),
-  "utf8",
-);
-
 export * from "./helpers.ts";
