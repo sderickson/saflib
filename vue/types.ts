6acff488aabafd563a18064afcfbe7579d823db9
Scott Erickson <sderickson@gmail.com>
2025-10-09 13:42:05 -0700

    Tweak vue loader types

--- vue/types.ts
+++ vue/types.ts
@@ -5,12 +5,12 @@ import type { Ref } from "vue";
 /**
  * A subset of what `useQuery` returns. This is so that loaders can create pseudo-queries by simply creating objects with isLoading, error, and isError properties.
  */
-export type LoaderQuery<TData = any> = Pick<
+export type LoaderQuery<TData = undefined> = Pick<
   UseQueryReturnType<any, TanstackError>,
   "isLoading" | "error"
 > & {
   isError: Ref<boolean>;
-  data: Ref<TData | undefined>;
+  data: TData;
 };
 
 /**
