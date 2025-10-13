592b2ef0e8b2cf6c27d8d6f63f1cb858999747e7
Scott Erickson <sderickson@gmail.com>
2025-10-13 10:39:44 -0700

    feat: create shared Address type in @saflib/drizzle

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -6,3 +6,4 @@ export {
 
 export { DbManager } from "./instances.ts";
 export * from "./types.ts";
+export type { Address } from "./types/address.ts";
---
+++ drizzle/types/address.ts
@@ -0,0 +1,8 @@
+export interface Address {
+  formatted: string | null;
+  street_address: string | null;
+  locality: string | null;
+  region: string | null;
+  country: string | null;
+  postal_code: string | null;
+}
