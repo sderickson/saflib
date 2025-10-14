023142cbb3706b559d05142d421c2d80e6ac91dd
Scott Erickson <sderickson@gmail.com>
2025-10-14 14:33:47 -0700

    migrate over snackbar queue

---
+++ vue/components/SnackbarQueue.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-snackbar-queue
+    v-model="errors"
+    color="error"
+    :timeout="5000"
+  ></v-snackbar-queue>
+  <v-snackbar-queue
+    v-model="info"
+    color="info"
+    :timeout="5000"
+  ></v-snackbar-queue>
+</template>
+
+<script setup lang="ts">
+import { errors, info } from "@saflib/vue";
+// TODO: Move this to @saflib/vue
+</script>
--- vue/components/index.ts
+++ vue/components/index.ts
@@ -1,3 +1,4 @@
 export { AsyncPage } from "./tricky-imports.ts";
 export { default as SpaLink } from "./SpaLink.vue";
 export { default as StubComponent } from "./StubComponent.vue";
+export { default as SnackbarQueue } from "./SnackbarQueue.vue";
