af740469304d52d10deab054c7c5c43fb7f80152
Scott Erickson <sderickson@gmail.com>
2025-10-17 17:56:39 -0700

    add top level container

---
+++ vue/components/TopLevelContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container class="mt-8">
+    <v-row>
+      <v-spacer />
+      <v-col cols="12" md="10" lg="6" xl="4">
+        <slot></slot>
+      </v-col>
+      <v-spacer />
+    </v-row>
+  </v-container>
+</template>
