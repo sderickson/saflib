e6bae4577940226aa7d5da20bd98949d1d146023
Scott Erickson <sderickson@gmail.com>
2025-10-23 09:09:01 -0700

    add add-job workflow

---
+++ cron/cron/workflows/add-job.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import { CronAddJobWorkflowDefinition } from "./add-job.ts";
+import { runWorkflow } from "@saflib/workflows";
+
+describe("add-job", () => {
+  it("should successfully dry run", async () => {
+    const result = await runWorkflow({
+      definition: CronAddJobWorkflowDefinition,
+      runMode: "dry",
+    });
+    expect(result?.checklist).toBeDefined();
+  });
+});
--- cron/cron/workflows/index.ts
+++ cron/cron/workflows/index.ts
@@ -1,13 +1,16 @@
 import { CronInitWorkflowDefinition } from "./init.ts";
+import { CronAddJobWorkflowDefinition } from "./add-job.ts";
 import type { WorkflowDefinition } from "@saflib/workflows";
 
 export {
   // Export each workflow definition separately
   CronInitWorkflowDefinition,
+  CronAddJobWorkflowDefinition,
 };
 
 const CronWorkflowDefinitions: WorkflowDefinition[] = [
   // And have the default export be the array of all of them
   CronInitWorkflowDefinition,
+  CronAddJobWorkflowDefinition,
 ];
 export default CronWorkflowDefinitions;
