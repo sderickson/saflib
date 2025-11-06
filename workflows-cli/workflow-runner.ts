98c8eeda94762eff7e6a21e3ac12235d6c7c4d3d
Scott Erickson <sderickson@gmail.com>
2025-11-06 15:30:22 -0800

    tweak runner

--- workflows-cli/workflow-runner.ts
+++ workflows-cli/workflow-runner.ts
@@ -2,16 +2,22 @@
 import { execSync } from "node:child_process";
 
 const endString = "--- This workflow has been completed. ---";
-const todoString = "it still contains TODO strings"
 
 export const runWorkflow = (workflow: string) => {
   execSync(`npm exec saf-workflow kickoff ${workflow} -- --skip-todos`, {
     stdio: "inherit",
   });
+  let lastOutput = "";
   while (true) {
     const output = execSync(`npm exec saf-workflow next`).toString();
     console.log(output);
-    if (output.includes(endString) || output.includes(todoString)) {
+    lastOutput = output;
+    if (output.includes(endString)) {
+      break;
+    }
+    if (output === lastOutput) {
+      console.log("Workflow appears stuck! Exiting.");
+      process.exit(1);
       break;
     }
   }
