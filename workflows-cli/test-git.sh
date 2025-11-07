fd0833e3e9eb1c977583cef8a98e20a9eaa868da
Scott Erickson <sderickson@gmail.com>
2025-11-06 18:07:06 -0800

    update

--- workflows-cli/test-git.sh
+++ workflows-cli/test-git.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# error if there are any git changes
+
+if git status -uall | grep -q "modified:"; then
+  echo "There are git changes. Please commit or stash them."
+  exit 1
+fi
+
 # Checkout a new branch
 current_branch=$(git branch --show-current)
 git checkout -b test-git
@@ -15,6 +22,9 @@ git log --oneline --left-right main...test-git
 # Go back, delete the branch
 git checkout $current_branch
 git branch -D test-git
+
+# Clear any git changes
+git reset --hard
 
 # Exit with the status code
 exit $status_code
