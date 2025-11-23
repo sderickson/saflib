b0953c3c0bf0033f32dde0bfa2f077bd83a23912
Scott Erickson <sderickson@gmail.com>
2025-11-23 07:18:43 -0800

    add sentry package, fix monolith

--- product/workflows/templates/services/__product-name__-monolith/run.ts
+++ product/workflows/templates/services/__product-name__-monolith/run.ts
@@ -3,7 +3,7 @@ import { addLokiTransport, collectSystemMetrics } from "@saflib/node";
 import { setServiceName } from "@saflib/node";
 import { validateEnv } from "@saflib/env";
 import envSchema from "./env.schema.combined.json" with { type: "json" };
-import { initSentry } from "@__organization-name__/sentry";
+import { initSentry } from "@saflib/sentry";
 
 validateEnv(process.env, envSchema);
 setServiceName("__product-name__");
