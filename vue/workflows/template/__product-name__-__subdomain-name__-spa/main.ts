848fdb769bffc5a625d5e358da0a3f1caa30db12
Scott Erickson <sderickson@gmail.com>
2025-11-28 10:18:54 -0800

    make spas be dynamic

--- vue/workflows/template/__product-name__-__subdomain-name__-spa/main.ts
+++ vue/workflows/template/__product-name__-__subdomain-name__-spa/main.ts
@@ -1,11 +1,12 @@
 import { createVueApp } from "@saflib/vue";
 import { setClientName } from "@saflib/links";
 import App from "./__SubdomainName__App.vue";
-import { router } from "./router.ts";
+import { create__SubdomainName__Router } from "./router.ts";
 import { __subdomain_name___strings } from "./strings.ts";
 
 export const main = () => {
   setClientName("__subdomain-name__");
+  const router = create__SubdomainName__Router();
   createVueApp(App, {
     router,
     i18nMessages: {
