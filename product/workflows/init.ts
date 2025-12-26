11bddae6e44c5d894252e321c5eccb3799a7019c
Scott Erickson <sderickson@gmail.com>
2025-12-26 09:09:54 -0800

    tweak

--- product/workflows/init.ts
+++ product/workflows/init.ts
@@ -19,8 +19,7 @@ import { IdentityInitWorkflowDefinition } from "@saflib/identity/workflows";
 const input = [
   {
     name: "name",
-    description:
-      "Name of the new product",
+    description: "Name of the new product",
     exampleValue: "foo",
   },
 ] as const;
@@ -35,8 +34,7 @@ export const InitProductWorkflowDefinition = defineWorkflow<
 >({
   id: "product/init",
 
-  description:
-    "Create a new product",
+  description: "Create a new product",
 
   input,
 
@@ -45,7 +43,7 @@ export const InitProductWorkflowDefinition = defineWorkflow<
   context: ({ input }) => {
     const packageName = getPackageName(input.cwd);
     const packageInfo = parsePackageName(packageName);
-    
+
     return {
       productName: input.name,
       sharedPackagePrefix: `@${packageInfo.organizationName}/${input.name}`,
@@ -56,8 +54,14 @@ export const InitProductWorkflowDefinition = defineWorkflow<
   },
 
   templateFiles: {
-    deploy: path.join(import.meta.dirname, "templates/deploy/__product-name__-dev"),
-    monolith: path.join(import.meta.dirname, "templates/services/__product-name__-monolith"),
+    deploy: path.join(
+      import.meta.dirname,
+      "templates/deploy/__product-name__-dev",
+    ),
+    monolith: path.join(
+      import.meta.dirname,
+      "templates/services/__product-name__-monolith",
+    ),
   },
 
   docFiles: {},
@@ -97,15 +101,18 @@ export const InitProductWorkflowDefinition = defineWorkflow<
       subdomainName: "account",
     })),
 
-    step(makeWorkflowMachine(IdentityInitWorkflowDefinition), ({ context }) => ({
-      name: `${context.sharedPackagePrefix}-identity`,
-      path: `./services/${context.productName}-identity`,
-    })),
+    step(
+      makeWorkflowMachine(IdentityInitWorkflowDefinition),
+      ({ context }) => ({
+        name: `${context.sharedPackagePrefix}-identity`,
+        path: `./services/${context.productName}-identity`,
+      }),
+    ),
 
     step(CopyStepMachine, ({ context }) => ({
       name: context.productName,
       targetDir: context.cwd,
-      lineReplace: makeLineReplace(context)
+      lineReplace: makeLineReplace(context),
     })),
 
     step(CommandStepMachine, () => ({
@@ -114,7 +121,10 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(CdStepMachine, ({ context }) => ({
-      path: path.join(context.cwd, `./services/${context.productName}-monolith`),
+      path: path.join(
+        context.cwd,
+        `./services/${context.productName}-monolith`,
+      ),
     })),
 
     step(CommandStepMachine, () => ({
@@ -136,10 +146,10 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `Set up the logged-out home page in the ${context.productName}-root-spa, integrating with the other spas.
+      prompt: `Set up the logged-out home page in the ${context.productName}-root-spa, integrating with the other spas.
       
       - Update the home page to have a call to action to the register page. Use linkToProps from @saflib/links to create the link and bind them to vuetify components.
-      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. This spa is "logged out".`
+      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. This spa is "logged out".`,
     })),
 
     step(CdStepMachine, ({ context }) => ({
@@ -147,12 +157,12 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `Set up the ${context.productName}-auth-spa, integrating with the other spas.
+      prompt: `Set up the ${context.productName}-auth-spa, integrating with the other spas.
       
       - Use the @saflib/auth package's router using 'createAuthRouter'. That will provide login, register, forgot password, and logout pages.
       - Include the @saflib/auth/strings in the auth-spa's strings file, so i18n works.
       - Make sure it redirects to the app spa's home page after login/register, using linkToHref from @saflib/links. And to root home page after logout.
-      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. The app will need to get the 'useProfile' hook from @saflib/auth and use it to determine if the user is logged in or not to give to the layout.`
+      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. The app will need to get the 'useProfile' hook from @saflib/auth and use it to determine if the user is logged in or not to give to the layout.`,
     })),
 
     step(CdStepMachine, ({ context }) => ({
@@ -160,9 +170,9 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `Set up the ${context.productName}-app-spa, integrating with the other spas.
+      prompt: `Set up the ${context.productName}-app-spa, integrating with the other spas.
       
-      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. The app is always logged in.`
+      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. The app is always logged in.`,
     })),
 
     step(CdStepMachine, ({ context }) => ({
@@ -170,12 +180,17 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `Set up the ${context.productName}-account-spa, integrating with the other spas.
+      prompt: `Set up the ${context.productName}-account-spa, integrating with the other spas.
       
       - Add to the router @saflib/account package's pages for changing password and updating profile.
       - Include the @saflib/account/strings in the account-spa's strings file, so i18n works.
       - Update the home page to link to those pages.
-      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. This spa is always logged in.`
+      - Incorporate the Layout exported from the ${context.sharedPackagePrefix}-clients-common package. This spa is always logged in.`,
+    })),
+
+    step(CommandStepMachine, () => ({
+      command: "npm",
+      args: ["run", "typecheck"],
     })),
 
     step(CdStepMachine, ({ context }) => ({
@@ -183,11 +198,11 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `Update the layout component in the ${context.sharedPackagePrefix}-clients-common package, adding links to the various spas.
+      prompt: `Update the layout component in the ${context.sharedPackagePrefix}-clients-common package, adding links to the various spas.
       
       - When logged out, link to the root spa's home page, and the auth spa's register page.
       - When logged in, link to the app spa's home page, the account spa's home page, and the auth spa's logout page.
-      - Also, if logged in as admin, link to the admin spa's home page.`
+      - Also, if logged in as admin, link to the admin spa's home page.`,
     })),
 
     step(CdStepMachine, ({ context }) => ({
@@ -200,12 +215,13 @@ export const InitProductWorkflowDefinition = defineWorkflow<
     })),
 
     step(PromptStepMachine, ({ context }) => ({
-      "prompt": `The dev environment is running on http://${context.productName}.docker.localhost. Please make sure it works.
+      prompt: `The dev environment is running on http://${context.productName}.docker.localhost. Please make sure it works.
       - Navigate to http://${context.productName}.docker.localhost and make sure it looks good.
       - Create a test account, make sure you end up on the app spa's home page.
       - Log out, make sure you end up back on the root spa's home page, then log in again.
       - Go to the account spa's home page, make sure you can change your password and update your profile.
-    `})),
+    `,
+    })),
   ],
 });
 
