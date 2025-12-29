63aaf56aa5528f2d440421f397050125419168aa
Scott Erickson <sderickson@gmail.com>
2025-12-29 07:41:59 -0800

    add a hack

--- product/workflows/init.ts
+++ product/workflows/init.ts
@@ -15,6 +15,7 @@ import { AddSpaWorkflowDefinition } from "@saflib/vue/workflows";
 import { InitServiceWorkflowDefinition } from "@saflib/service/workflows";
 import path from "node:path";
 import { IdentityInitWorkflowDefinition } from "@saflib/identity/workflows";
+import fs from "node:fs";
 
 const input = [
   {
@@ -64,6 +65,27 @@ export const InitProductWorkflowDefinition = defineWorkflow<
   },
 
   steps: [
+    step(CommandStepMachine, ({ context }) => {
+      // hack to add the product to the workspaces w/out deps
+      // probably the makings of a new workflow step here
+      const packageJson = JSON.parse(
+        fs.readFileSync(path.join(context.cwd, "package.json"), "utf8"),
+      );
+      const newWorkspaces = [
+        ...packageJson.workspaces,
+        `${context.productName}/**`,
+      ];
+      newWorkspaces.sort();
+      packageJson.workspaces = newWorkspaces;
+      fs.writeFileSync(
+        path.join(context.cwd, "package.json"),
+        JSON.stringify(packageJson, null, 2),
+      );
+      return {
+        command: "npm",
+        args: ["exec", "prettier", "--", "package.json", "--write"],
+      };
+    }),
     step(makeWorkflowMachine(InitServiceWorkflowDefinition), ({ context }) => ({
       name: `${context.sharedPackagePrefix}-service`,
       path: `./${context.productName}/service`,
