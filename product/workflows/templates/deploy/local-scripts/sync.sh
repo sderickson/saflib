6d8676dd58fccf51fc781edeba851e95a3c87fd1
Scott Erickson <sderickson@gmail.com>
2026-01-01 08:49:52 -0800

    fix sync

--- product/workflows/templates/deploy/local-scripts/sync.sh
+++ product/workflows/templates/deploy/local-scripts/sync.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-REMOTE_HOST=$SSH_HOSTNAME # e.g. root@<server-domain>
-REMOTE_ZIP_PATH=$REMOTE_ZIP_PATH # where to put the zip file, e.g. `/root/`
-REMOTE_ASSETS_FOLDER_PATH=$REMOTE_ASSETS_FOLDER_PATH # where to put the extracted files, e.g. `/root/your-project/`
+source ./deploy/env.remote
+echo "Remote host: $SSH_HOSTNAME"
+echo "Remote zip path: $REMOTE_ZIP_PATH"
+echo "Remote assets folder path: $REMOTE_ASSETS_FOLDER_PATH"
+
 LOCAL_FOLDER="./deploy/remote-assets"
 ZIP_NAME="deploy-instance.zip"
 
-# Create zip file
+# Create zip file with contents of remote-assets (not the directory itself)
 echo "Creating zip file..."
-zip -r "$ZIP_NAME" "$LOCAL_FOLDER"
+(cd "$LOCAL_FOLDER" && zip -r "../../$ZIP_NAME" .)
 
 # Push file via sftp
 echo "Transferring zip file..."
-sftp "$REMOTE_HOST" << EOF
-put "$ZIP_NAME" "$REMOTE_PATH/$ZIP_NAME"
+sftp "$SSH_HOSTNAME" << EOF
+put "$ZIP_NAME" "$REMOTE_ZIP_PATH/$ZIP_NAME"
 exit
 EOF
 
 # SSH into remote, unzip file and run command
 echo "Unzipping and running command on remote..."
-ssh "$REMOTE_HOST" << EOF
+ssh "$SSH_HOSTNAME" << EOF
 sudo -i
-cd "$REMOTE_PATH"
+cd "$REMOTE_ZIP_PATH"
 # Check if unzip is installed, if not install it
 if ! command -v unzip &> /dev/null; then
     echo "unzip not found, installing..."
     apt-get update
     apt-get install -y unzip
 fi
-mkdir -p "$REMOTE_FOLDER"
+mkdir -p "$REMOTE_ASSETS_FOLDER_PATH"
 echo "Removing existing files..."
-echo "Removing $REMOTE_FOLDER*"
-rm -rf $REMOTE_FOLDER* -v
-unzip -o "$ZIP_NAME" -d "$REMOTE_FOLDER"
+echo "Removing $REMOTE_ASSETS_FOLDER_PATH*"
+rm -rf $REMOTE_ASSETS_FOLDER_PATH* -v
+unzip -o "$ZIP_NAME" -d "$REMOTE_ASSETS_FOLDER_PATH"
 rm "$ZIP_NAME"
 echo "Done!"
 EOF
