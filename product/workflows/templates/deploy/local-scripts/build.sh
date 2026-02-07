d9d6c921a08c42719f9b68e29a43c44c8ce69479
Scott Erickson <sderickson@gmail.com>
2026-02-07 09:49:34 -0800

    fix prod deploy in product/init

--- product/workflows/templates/deploy/local-scripts/build.sh
+++ product/workflows/templates/deploy/local-scripts/build.sh
@@ -5,8 +5,11 @@ source ./deploy/env.remote
 echo "Container registry: $CONTAINER_REGISTRY"
 
 # Build dependent images
+# BEGIN WORKFLOW AREA build-product-dependencies FOR product/init
 docker build -t __organization-name__-__product-name__-clients:latest -f ./__product-name__/clients/build/Dockerfile . --platform linux/amd64
 docker build -t $CONTAINER_REGISTRY/__organization-name__-__product-name__-monolith:latest -f ./__product-name__/service/monolith/Dockerfile . --platform linux/amd64
+
+# END WORKFLOW AREA
 
 # Build reverse proxy image
 docker build -t $CONTAINER_REGISTRY/__organization-name__-caddy:latest -f ./deploy/Dockerfile.prod . --platform linux/amd64
