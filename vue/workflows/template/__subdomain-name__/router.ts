02cc80e9405f92533737d7abcc17457a2cfda1a6
Scott Erickson <sderickson@gmail.com>
2026-02-13 15:19:28 -0800

    improve things

--- vue/workflows/template/__subdomain-name__/router.ts
+++ vue/workflows/template/__subdomain-name__/router.ts
@@ -6,15 +6,15 @@ import { PageNotFound } from "@saflib/vue/components";
 console.log("__subdomainName__Links:", __subdomainName__Links);
 
 // BEGIN SORTED WORKFLOW AREA page-imports FOR vue/add-view
-import __TargetName__Async from "./__group-name__/__TargetName__Async.vue";
+import __FullName__Async from "./__group-name__/__TargetName__Async.vue";
 // END WORKFLOW AREA
 
 export const create__SubdomainName__Router = () => {
   const routes = [
     // BEGIN WORKFLOW AREA page-routes FOR vue/add-view
     {
-      path: __subdomainName__Links.__target_name__.path,
-      component: __TargetName__Async,
+      path: __subdomainName__Links.__fullName__.path,
+      component: __FullName__Async,
     },
     // END WORKFLOW AREA
     { path: "/:pathMatch(.*)*", component: PageNotFound },
