05dc69fa4f677acf3d9036f075da6d2e2c9cd95d
Scott Erickson <sderickson@gmail.com>
2026-02-16 14:58:02 -0800

    add missing push workflow area

--- product/workflows/templates/deploy/local-scripts/push.sh
+++ product/workflows/templates/deploy/local-scripts/push.sh
@@ -5,4 +5,6 @@ source ./deploy/env.remote
 echo "Container registry: $CONTAINER_REGISTRY"
 
 docker push $CONTAINER_REGISTRY/__organization-name__-caddy:latest
+# BEGIN WORKFLOW AREA push-images FOR product/init
 docker push $CONTAINER_REGISTRY/__organization-name__-__product-name__-monolith:latest
+# END WORKFLOW AREA
