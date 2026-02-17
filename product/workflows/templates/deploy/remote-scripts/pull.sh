f12a06c5217440aa9686d9008df969816daeec45
Scott Erickson <sderickson@gmail.com>
2026-02-16 17:38:18 -0800

    Improve auto-generation of deploy dir in product/init (#206)
    In getting deploys to work for a product, I found some more missing
    issues with the product/init deploy directory. These are fixes for them.

--- product/workflows/templates/deploy/remote-scripts/pull.sh
+++ product/workflows/templates/deploy/remote-scripts/pull.sh
@@ -1,5 +1,7 @@
 
 echo "Pulling latest docker images..."
 docker pull $CONTAINER_REGISTRY/__organization-name__-caddy:latest
+# BEGIN WORKFLOW AREA pull-images FOR product/init
 docker pull $CONTAINER_REGISTRY/__organization-name__-__product-name__-monolith:latest
+# END WORKFLOW AREA
 echo "Done!"
