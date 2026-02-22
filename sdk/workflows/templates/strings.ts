4896ff9412c88902b5bbc071c4b2b244988c1c6d
Scott Erickson <sderickson@gmail.com>
2026-02-22 08:14:50 -0800

    make consistent

--- sdk/workflows/templates/strings.ts
+++ sdk/workflows/templates/strings.ts
@@ -1,9 +1,9 @@
-// BEGIN SORTED WORKFLOW AREA string-exports FOR sdk/add-component
+// BEGIN SORTED WORKFLOW AREA string-imports FOR vue/add-view sdk/add-component
 import { __full_name___strings } from "./__group-name__/__TargetName__.strings.ts";
 // END WORKFLOW AREA
 
 export const __service_name___sdk_strings = {
-  // BEGIN SORTED WORKFLOW AREA string-object FOR sdk/add-component
+  // BEGIN SORTED WORKFLOW AREA string-object FOR vue/add-view sdk/add-component
   __full_name___strings,
   // END WORKFLOW AREA
 };
