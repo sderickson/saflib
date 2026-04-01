7f77d13210e872006e6386398afea751f8487b23
Scott Erickson <sderickson@gmail.com>
2026-04-01 16:46:54 -0700

    fix

--- vue/workflows/template/__subdomain-name__/__group-name__/__TargetName__.vue
+++ vue/workflows/template/__subdomain-name__/__group-name__/__TargetName__.vue
@@ -3,30 +3,30 @@
     <h1>{{ t(strings.title) }}</h1>
     <v-text-field v-bind="t(strings.example_input)"></v-text-field>
     <i18n-t
-      v-if="profile.id"
+      v-if="identity"
       scope="global"
       :keypath="lookupTKey(strings.logged_in_with_email)"
     >
-      <template #email>{{ profile.email }}</template>
+      <template #email>{{ identity.traits.email }}</template>
     </i18n-t>
     <div v-else>{{ t(strings.not_logged_in) }}</div>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import type { Identity } from "@ory/client";
 import { __full_name__ as strings } from "./__TargetName__.strings.ts";
 import { use__TargetName__Loader } from "./__TargetName__.loader.ts";
 import { useReverseT } from "template-package-spa/i18n";
 
 const { t, lookupTKey } = useReverseT();
 
-const { profileQuery } = use__TargetName__Loader();
+const { sessionQuery } = use__TargetName__Loader();
 
 // the Async component will not render if the data is not loaded
 // check to make sure the data is loaded before rendering
-if (!profileQuery.data.value) {
-  throw new Error("Failed to load profile");
+let identity: Identity | undefined;
+if (sessionQuery.data.value?.identity) {
+  identity = sessionQuery.data.value.identity;
 }
-
-const profile = profileQuery.data.value;
 </script>
