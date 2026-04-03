f1a13845969ae521b997ebc07eaa7a871120e989
Scott Erickson <sderickson@gmail.com>
2026-04-02 20:10:25 -0700

    fix

--- vue/workflows/template/common/playwright.config.ts
+++ vue/workflows/template/common/playwright.config.ts
@@ -1,6 +1,6 @@
 // Hack - load env a better way, but not dotenv, it's endless pushing dotenvx
 process.env.DOMAIN = "docker.localhost";
 process.env.PROTOCOL = "http";
-process.env.SERVICE_SUBDOMAINS = "identity,__product-name__";
+process.env.SERVICE_SUBDOMAINS = "__product-name__";
 
 export { default } from "@saflib/playwright/playwright.config";
