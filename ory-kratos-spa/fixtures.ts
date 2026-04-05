824f55512ec379f36d8b9d0df1c407ed349440a6
Scott Erickson <sderickson@gmail.com>
2026-04-05 11:04:45 -0700

    add login fixture

--- ory-kratos-spa/fixtures.ts
+++ ory-kratos-spa/fixtures.ts
@@ -1,6 +1,10 @@
 // Re-export fixtures for each page here.
 // BEGIN SORTED WORKFLOW AREA fixture-exports FOR vue/add-view
 export {
+  LoginPageFixture,
+  loginPageFixture,
+} from "./pages/login/login.fixture.ts";
+export {
   RegistrationPageFixture,
   registrationPageFixture,
 } from "./pages/registration/registration.fixture.ts";
