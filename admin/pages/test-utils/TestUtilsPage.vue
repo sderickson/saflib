6dba65ad68cc908fb62f007d034461437c447ea9
Scott Erickson <sderickson@gmail.com>
2026-04-08 19:04:27 -0700

    Add git hashes to test utils (#275)
    Going to want to rethink the whole admin router at some point but for
    now throwing this in there.

--- admin/pages/test-utils/TestUtilsPage.vue
+++ admin/pages/test-utils/TestUtilsPage.vue
@@ -1,11 +1,38 @@
 <template>
-  <v-container>
+  <v-container class="pa-4">
     <h1 class="mt-b">{{ t(strings.title) }}</h1>
-    <v-row>
+    <v-row class="py-4">
       <v-col cols="auto">
         <v-btn color="primary" @click="enterTestMode">
           {{ t(strings.enterTestMode) }}
         </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="pa-4">
+          <v-card-title>Git Hashes</v-card-title>
+          <v-table>
+            <thead>
+              <tr>
+                <th>Key</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Root</td>
+                <td>{{ gitHashes.root }}</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Saflib</td>
+                <td>{{ gitHashes.saflib }}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -15,6 +42,9 @@
 import { test_utils_page as strings } from "./TestUtilsPage.strings";
 import { setTestMode } from "@saflib/vue";
 import { useReverseT } from "../../i18n";
+import { getGitHashes } from "@saflib/vue";
+
+const gitHashes = getGitHashes();
 
 const { t } = useReverseT();
 
