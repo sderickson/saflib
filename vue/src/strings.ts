20ec97d9fd0c7e771838b96355ac3c9bce075499
Scott Erickson <sderickson@gmail.com>
2026-04-30 10:49:02 -0700

    extend makeReverseTComposable

--- vue/src/strings.ts
+++ vue/src/strings.ts
@@ -60,13 +60,20 @@ export const makeReverseTComposable = (strings: I18nMessages) => {
     };
     // Function overloads to preserve input type
     function wrappedT(s: string): string;
+    function wrappedT(s: string, values: Record<string, unknown>): string;
     function wrappedT(s: I18NObject): I18NObject;
-    function wrappedT(s: string | I18NObject): string | I18NObject {
+    function wrappedT(
+      s: string | I18NObject,
+      values?: Record<string, unknown>,
+    ): string | I18NObject {
       if (typeof s === "string") {
-        return stringToKeyMap.get(s) ? t(lookupTKey(s)) : s;
-      } else {
-        return tObject(s);
+        if (!stringToKeyMap.get(s)) return s;
+        const key = lookupTKey(s);
+        return values !== undefined
+          ? (t(key, values) as string)
+          : (t(key) as string);
       }
+      return tObject(s);
     }
     const tObject = (o: I18NObject): I18NObject => {
       return Object.fromEntries(
