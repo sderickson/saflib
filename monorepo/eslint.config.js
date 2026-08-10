962e9e52de4bb13aef375438fddddf88198b4603
Scott Erickson <sderickson@gmail.com>
2026-08-10 10:39:19 -0700

    fix eslint

--- monorepo/eslint.config.js
+++ monorepo/eslint.config.js
@@ -1,8 +1,12 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginVue from "eslint-plugin-vue";
 import globals from "globals";
 import typescriptEslint from "typescript-eslint";
+
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 // TODO: Add linters for everything other than "clients"
 
@@ -19,7 +23,10 @@ export const config = typescriptEslint.config(
       ecmaVersion: "latest",
       sourceType: "module",
       globals: { ...globals.browser },
-      parserOptions: { parser: typescriptEslint.parser },
+      parserOptions: {
+        parser: typescriptEslint.parser,
+        tsconfigRootDir,
+      },
     },
     rules: {
       // Allow unused variables when they are prefixed with "_"
