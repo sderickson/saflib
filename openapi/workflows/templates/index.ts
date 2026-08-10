bc1925170811966a87e89947e37ef8f356623f35
Scott Erickson <sderickson@gmail.com>
2026-08-10 15:17:39 -0700

    remove barrel exports from spec template

--- openapi/workflows/templates/index.ts
+++ openapi/workflows/templates/index.ts
@@ -1,22 +1,10 @@
 import type { components, operations } from "./dist/openapi.d.ts";
 
 export type { paths } from "./dist/openapi.d.ts";
-import {
-  type ExtractResponseBody,
-  type ExtractRequestBody,
-  castJson,
-} from "@saflib/openapi";
-
-export type __ServiceName__ServiceResponseBody =
-  ExtractResponseBody<operations>;
-export type __ServiceName__ServiceRequestBody = ExtractRequestBody<operations>;
+import { castJson } from "@saflib/openapi";
 
 export type Error = components["schemas"]["Error"];
 export type ProductEvent = components["schemas"]["ProductEvent"];
-
-// BEGIN WORKFLOW AREA schema-exports FOR openapi/schema
-export type __TargetName__ = components["schemas"]["__TargetName__"];
-// END WORKFLOW AREA
 
 import * as json from "./dist/openapi.json" with { type: "json" };
 
