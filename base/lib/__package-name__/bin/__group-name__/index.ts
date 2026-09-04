ea87d708872e3f357a1cfd257a670b50874e0c92
Scott Erickson <sderickson@gmail.com>
2026-09-04 07:48:06 -0700

    refactor bin commands to be standard

--- base/lib/__package-name__/bin/__group-name__/index.ts
+++ base/lib/__package-name__/bin/__group-name__/index.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env -S node --experimental-strip-types --disable-warning=ExperimentalWarning
 import { Command } from "commander";
 import { setupContext } from "@saflib/commander";
+import { add__TargetName__Command } from "./__target-name__.ts";
 
 const program = new Command()
   .name("template-file")
   .description("TODO: Add CLI description");
+
+add__TargetName__Command(program);
 
 setupContext(
   {
