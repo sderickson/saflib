4a6e73c38c070d6eb3f8e13f10f6930908c08a5f
Scott Erickson <sderickson@gmail.com>
2026-09-21 19:06:35 -0700

    fix

--- base/service/db/schema.ts
+++ base/service/db/schema.ts
@@ -1,4 +1,4 @@
-// BEGIN WORKFLOW AREA schema-exports FOR drizzle/update-schema
+// BEGIN SORTED WORKFLOW AREA schema-exports FOR drizzle/update-schema
 export * from "./schemas/__group-name__.ts";
 // END WORKFLOW AREA
 
